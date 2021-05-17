da5777f810c2b93d32b7acc8d2b43114ef43667a
Michael Chen <atlasneiko@gmail.com>
2021-05-17 11:30:20 -0700

    setup MongoDB

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const db = require('./config/keys').mongoURI;
+const passport = require('passport');
+const path = require('path');
+
+//* import routes here
+
+//* connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("connect to MongoDB successfully!"))
+  .catch(err => console.log(err))
+
+//* setup poassport
+app.use(passport.initialize());
+// require("./config/passport")(passport);
+const port = process.env.PORT || 5000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,9 @@
+const database = "achieved";
+const username = "achievedDev";
+const password = "z.G-Fn6GG4C8bf5";
+
+module.exports = {
+  mongoURI: `mongodb+srv://${username}:${password}@cluster0.mo6di.mongodb.net/${database}?retryWrites=true&w=majority`,
+  // mongoURI: `mongodb+srv://${username}:${password}@cluster0.qyqvz.mongodb.net/${database}?retryWrites=true&w=majority`,
+  // secretOrKey: 'secret',
+};
